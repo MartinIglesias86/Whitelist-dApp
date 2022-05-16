76b993d0061b3f18f2855f9547ea5eae25a7c3bd
Martin Iglesias <72715575+MartinIglesias86@users.noreply.github.com>
2022-05-16 16:36:01 -0300

    First commit - Whitelist dApp

---
+++ constants/index.js
@@ -0,0 +1,66 @@
+export const abi = [
+	{
+		"inputs": [],
+		"name": "addAddressToWhitelist",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint8",
+				"name": "_maxWhitelistedAddresses",
+				"type": "uint8"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [],
+		"name": "maxWhitelistedAddresses",
+		"outputs": [
+			{
+				"internalType": "uint8",
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "numAddressesWhitelisted",
+		"outputs": [
+			{
+				"internalType": "uint8",
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "whitelistedAddresses",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+export const WHITELIST_CONTRACT_ADDRESS = "0x8e4e5184c20bb65415234d077adeaab71a6fba7b";
